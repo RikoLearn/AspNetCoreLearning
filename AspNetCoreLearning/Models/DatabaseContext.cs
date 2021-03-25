6c341732e3032e2385deebc1523ebd2e9f26c3cc
Reza Kazem Nia <kazemi.reza76@yahoo.com>
2021-03-25 17:52:19 +0430

    Database Context
    Install Package EntityFrameWorkCore....

---
+++ AspNetCoreLearning/Models/DatabaseContext.cs
@@ -0,0 +1,11 @@
+﻿namespace AspNetCoreLearning.Models
+{
+    public class DatabaseContext: Microsoft.EntityFrameworkCore.DbContext
+    {
+        public DatabaseContext(Microsoft.EntityFrameworkCore.DbContextOptions<DatabaseContext> options):base(options)
+        {
+        }
+
+
+    }
+}
