8fbaf0a132496162d20fa64d1e2963e21b4c6113
Reza Kazem Nia <kazemi.reza76@yahoo.com>
2021-03-25 17:26:29 +0430

    Book Model Connection String in appsetting.json

---
+++ AspNetCoreLearning/Models/Book.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace AspNetCoreLearning.Models
+{
+    public class Book
+    {
+        public Book()
+        {
+
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
